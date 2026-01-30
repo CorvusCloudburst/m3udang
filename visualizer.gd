4f80db3a54b81249120ab4652c69754e1566ca8b
Corvus Cloudburst <dev.willfully192@passmail.net>
2026-01-29 23:02:58 -0500

    update layout ratio

--- visualizer.gd
+++ visualizer.gd
@@ -3,6 +3,7 @@
 
 extends PanelContainer
 
+# ---------------------------------------
 const BAR_COUNT: int = 32
 const FREQ_MAX: float = 11050.0
 const MIN_DB: int = 60
@@ -11,16 +12,19 @@ var spectrum: AudioEffectSpectrumAnalyzerInstance
 var bars: Array[Height] = []
 var bar_width: float = 0.0
 
+# ---------------------------------------
 func _ready() -> void:
 	spectrum = AudioServer.get_bus_effect_instance(0,0)
 	_on_resized()
 	for i: int in BAR_COUNT:
 		bars.append(Height.new())
-	
+
+# ---------------------------------------
 func _process(_delta: float) -> void:
 	_update_spectrum_data()
 	queue_redraw()
-	
+
+# ---------------------------------------
 func _draw() -> void:
 	for i: int in BAR_COUNT:
 		var color: Color = Color.from_hsv((BAR_COUNT * 0.6 + i * 0.5) / BAR_COUNT, 0.4, 0.7, 0.7)
@@ -31,7 +35,8 @@ func _draw() -> void:
 			bars[i].actual            # Height
 		)
 		draw_rect(rectangle, color)
-	
+
+# ---------------------------------------
 func _update_spectrum_data() -> void:
 	for i: int in BAR_COUNT:
 		# Determine the frequency range
@@ -54,11 +59,11 @@ func _update_spectrum_data() -> void:
 		# Apply the change
 		bars[i].actual = lerp(bars[i].low, bars[i].high, 0.1)
 
+# ---------------------------------------
 func _on_resized() -> void:
 	bar_width = size.x / BAR_COUNT
-	print(size)
-	print(size.x)
 
+# ---------------------------------------
 class Height:
 	var high: float
 	var low: float
