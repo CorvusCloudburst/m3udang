37f828af6c5d51e1c62095f119595d7627ef185e
Corvus Cloudburst <dev.willfully192@passmail.net>
2026-04-19 16:53:18 -0400

    Complete rewrite

---
+++ app/playlist_management/playlist_list_item.gd
@@ -0,0 +1,31 @@
+class_name PlaylistListItem
+extends Control
+
+# -----------------------------------------------------------------
+# Setup
+# -----------------------------------------------------------------
+# Provided parameter
+@export var filename: String = 'playlist.m3u'
+
+# Display element
+@onready var playlist_name_label: Label = $Panel/OuterMargins/PanelLayoutBox/PlaylistNameLabel
+
+# Setup on instantiation
+func _ready() -> void:
+	# Set the label text
+	var title = filename.get_file().rsplit(".", false, 1)[0]
+	playlist_name_label.text = title
+	
+# -----------------------------------------------------------------
+# Interactivity
+# -----------------------------------------------------------------
+
+# ------------- Open playlist details -------------
+func _on_open_detail_button_pressed() -> void:
+	# Announce to ancestors that the details should open
+	open_playlist_detail.emit(filename)
+
+# -----------------------------------------------------------------
+# External communication
+# -----------------------------------------------------------------
+signal open_playlist_detail(playlist_filename: String)
--- scripts/time_slider.gd
+++
@@ -1,9 +0,0 @@
-extends HSlider
-
-var dragging: bool
-
-func _process(_delta: float) -> void:
-	if !dragging:
-		min_value = 0
-		max_value = Globals.songLength
-		value = Globals.seconds
